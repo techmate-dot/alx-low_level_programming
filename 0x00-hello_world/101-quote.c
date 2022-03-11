4595428ff4515a11a3de310db257c0d28878e6b1
techmate-dot <snwankwo218@gmail.com>
2022-03-10 19:41:22 -0800

    working

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main - funtion
-*
-*Return: 1 if successful
+*Return: 1 if
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "\n");
 return (1);
 }
