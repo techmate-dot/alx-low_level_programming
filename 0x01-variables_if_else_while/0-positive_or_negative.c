60ec84929787287417f48e35d974e4b534af3c51
techmate-dot <snwankwo218@gmail.com>
2022-03-12 02:49:00 +0000

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+printf("%d positive\n", n);
 }
 else if (n == 0)
 {
-printf("is zero\n");
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("is negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
