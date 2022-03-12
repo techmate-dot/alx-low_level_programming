5bf4b83e87ca4c70bfdd58fc28d3a668198be078
techmate-dot <snwankwo218@gmail.com>
2022-03-12 05:47:34 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+int num = 48;
+while (num < 58)
+{
+putchar(num);
+if (num != 57)
+putchar(',');
+putchar(' ');
+num++;
+}
+putchar('\n');
+return (0);
+}
