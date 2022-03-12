02687b7261d4114bcc7e8c0c20b7021a0a088857
techmate-dot <snwankwo218@gmail.com>
2022-03-12 03:34:42 +0000

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is 0\n", n, s);
 }
 else if (s < 6 && s != 0)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, s);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 }
 return (0);
 }
