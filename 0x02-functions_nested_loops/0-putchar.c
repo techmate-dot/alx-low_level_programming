ff94599f3f3283f38fb92785720bcadd887cc8de
techmate-dot <snwankwo218@gmail.com>
2022-03-15 20:14:56 +0000

    0. _putchar mandatory Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
 #include "main.h"
 /**
-*main - all programs run from witin this function
-*
-*Return: return 0 if the program is good _putchar
-*/
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return: 0 after completion.
+ */
 int main(void)
 {
-int size = 8;
-int name[size];
-name[0] = '_';
-name[1] = 'p';
-name[2] = 'u';
-name[3] = 't';
-name[4] = 'c';
-name[5] = 'h';
-name[6] = 'a';
-name[7] = 'r';
-for (int i = 0; i < 8; i++)
-{
-_putchar(name[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
