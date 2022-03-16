d339280efefe530984472e9fb955ee73fedc116e
techmate-dot <snwankwo218@gmail.com>
2022-03-16 14:23:12 +0000

    TASK 3 Write a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*_islower - entry point
+*
+*Returns: returns 0 or 1
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <=122)
+return (1);
+else if (c >= 65 && c <=90)
+return (0);
+}
