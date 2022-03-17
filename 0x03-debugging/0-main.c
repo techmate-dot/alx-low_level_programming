cd77cca61913e0d29dfbf824c29089f51351acef
techmate-dot <snwankwo218@gmail.com>
2022-03-17 11:16:22 +0000

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - main entry of the code
+*Return: returns Zero always
+*/
+int main(void)
+{
+int n;
+n = 0;
+positive_or_negative(n);
+return (0);
+}
