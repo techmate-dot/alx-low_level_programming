f02979cd5584bb3e6ccb1cece914856a3b055279
techmate-dot <snwankwo218@gmail.com>
2022-03-17 16:18:08 +0000

    Task 2 This program prints the largest of three integers.

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,10 +7,16 @@
 
 int main(void)
 {
-        int i;
+        int a, b, c;
+        int largest;
 
-        i = 98;
-        positive_or_negative(i);
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
 
         return (0);
 }
