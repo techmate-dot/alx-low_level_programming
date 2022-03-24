42ff3563879ca66c8bc83d6bfb8bcaa844e68a6f
techmate-dot <snwankwo218@gmail.com>
2022-03-24 16:51:37 +0000

    Task 2 Write a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 *_strncpy - A function that copies a string
 *Return: returns the pointer the new string
 *@dest: arguement of the string that will be formed
-*src: Arguement contaiing the astring that will be copied fron
+*@src: Arguement contaiing the astring that will be copied fron
 *@n: How much string will be copied
 */
 char *_strncpy(char *dest, char *src, int n)
