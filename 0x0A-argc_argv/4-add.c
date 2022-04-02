471d9bf3c45b85fb6d1279eda47e1ac394cf6c33
techmate-dot <snwankwo218@gmail.com>
2022-04-02 00:06:00 +0000

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints all arguments it receives.
+*@c: counter
+*@v: vector
+*Return: returns all arguements
+*/
+
+int main(int c, char *v[])
+{
+int count = 0;
+int n = 1;
+
+if (c <= 1)
+{
+printf("%i\n", 0);
+}
+while (n < c)
+{
+count += atoi(v[n]);
+if (atoi(v[n]) == 0)
+{
+printf("Error\n");
+return (1);
+break;
+}
+n++;
+}
+if (atoi(v[n - 1]) != 0)
+{
+printf("%i\n", count);
+}
+return (0);
+}
